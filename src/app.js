a158d22f7794cb9e9d17a6e65e6789e0a447fadf
abdesselam <abdesselam.mihoubi@gmail.com>
2019-02-06 12:10:57 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -1,36 +1,40 @@
-const path = require('path');
-const express = require('express');
-const mongoose = require('mongoose');
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+
+var cors = require("cors");
 
 const app = express();
 
 // connection to db
-mongoose.connect('mongodb://localhost/sig-eadn')
-  .then(db => console.log('db connected'))
+mongoose
+  .connect("mongodb://localhost/sig-eadn")
+  .then(db => console.log("db connected"))
   .catch(err => console.log(err));
 
-  // importing routes
-const RestDjezzyRoutes = require('./routes/RestDjezzy');
-const RestAlgeriePoste = require('./routes/RestAlgeriePoste');
-const RestOoredoo = require('./routes/RestOoredoo');
-const RestLTE = require('./routes/RestLTE');
-const RestMSAN = require('./routes/RestMSAN');
-const RestLFO = require('./routes/RestLFO');
+app.use(cors({ origin: "http://localhost:8080" }));
+
+// importing routes
+const RestDjezzyRoutes = require("./routes/RestDjezzy");
+const RestAlgeriePoste = require("./routes/RestAlgeriePoste");
+const RestOoredoo = require("./routes/RestOoredoo");
+const RestLTE = require("./routes/RestLTE");
+const RestMSAN = require("./routes/RestMSAN");
+const RestLFO = require("./routes/RestLFO");
 
 // settings
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-app.use(express.json())
- 
-// routes 
-app.use('/', RestDjezzyRoutes);
-app.use('/', RestAlgeriePoste);
-app.use('/', RestOoredoo);
-app.use('/', RestLTE); 
-app.use('/', RestMSAN);
-app.use('/', RestLFO);
+app.use(express.json());
 
-app.listen(app.get('port'), () => {
-  console.log(`server on port ${app.get('port')}`);
+// routes
+app.use("/", RestDjezzyRoutes);
+app.use("/", RestAlgeriePoste);
+app.use("/", RestOoredoo);
+app.use("/", RestLTE);
+app.use("/", RestMSAN);
+app.use("/", RestLFO);
+
+app.listen(app.get("port"), () => {
+  console.log(`server on port ${app.get("port")}`);
 });
- 
